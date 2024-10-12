ba29f75f6d4f8f3042485d79fe2323b419e50723
ll12-cmd <liuli15761474286@163.com>
2024-10-12 15:57:22 +0800

    Add files via upload

---
+++ Filter作业/filter-login/src/main/java/gzu/User.java
@@ -0,0 +1,20 @@
+package gzu;
+
+public class User {
+    //定义用户名和密码
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ Filter作业/filter-login/src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>首页</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+<h1>欢迎来到首页</h1>
+<a href="login.html">登录</a>
+<a href="register.html">注册</a>
+</body>
+</html>
